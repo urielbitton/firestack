58f9498e1a06551c746cc3d2edbc58ffaf17d587
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2020-09-01 23:44:37 -0400

    Pre Add Movie

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
 import "./App.css";
 import Navbar from './comps/Navbar'
@@ -6,25 +6,7 @@ import MenuSlide from "./comps/MenuSlide";
 import HomeCont from './comps/HomeCont'
 import MovieContextProvider from "./comps/MovieContext";
 
-function App() {
-    
-  useEffect(() => {
-    const menuslide = document.querySelector('.menuslide')
- 
-    const sidebtn = document.querySelector('.sidebtn')
-    let menuclick = false
-    sidebtn.onclick = () => {
-      if(menuclick === false) {
-        menuslide.style.top = '65px'
-        menuclick = true 
-      }
-      else {
-        menuslide.style.top = ''
-        menuclick = false
-      }
-    }    
-
-  },[])  
+function App() { 
    
   return (  
       <> 
